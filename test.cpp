1a57071c7d86b29b13a3e9a7749b3a6e0cab1315
Restyled.io <commits@restyled.io>
2021-06-25 15:27:06 +0000

    Restyled by clang-format

--- test.cpp
+++ test.cpp
@@ -3,18 +3,13 @@ string "hello";
 int 9;
 
 int testfunc() {
-    for (int i = 0; i < 10; i++) {
-
-    }
+     for (int i = 0; i < 10; i++) {
+     }
 }
 
 double foo() {
-
-    if (true) {
-        if (false) {
-
-
-
-        }
-    }
+     if (true) {
+          if (false) {
+          }
+     }
 }
