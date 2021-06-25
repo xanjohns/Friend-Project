376f6994529b1fb478a70b986b68751131ebecb1
Xan Johnson <xanjohns@gmail.com>
2021-06-25 09:25:54 -0600

    Modified test file

--- test.cpp
+++ test.cpp
@@ -1,3 +1,20 @@
    int 2; 
  string "hello";
              int 9;
+
+ int testfunc() {
+          for (int i = 0; i < 10; i++) {
+
+}
+}
+
+      double foo() {
+
+	if (true) {
+if (false){
+
+
+
+}
+}
+}
